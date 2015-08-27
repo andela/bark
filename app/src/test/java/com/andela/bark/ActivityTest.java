3409b2d5ed39d402f16e953508d708841fd34f4e
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-08-27 14:51:21 +0100

    testLandingPage contains app log [pass]

--- app/src/test/java/com/andela/bark/ActivityTest.java
+++ app/src/test/java/com/andela/bark/ActivityTest.java
@@ -3,6 +3,9 @@ package com.andela.bark;
 /**
  * Created by andela-cj on 8/26/15.
  */
+import android.widget.ImageView;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,4 +20,21 @@ import static org.assertj.android.api.Assertions.assertThat;
 @Config(constants = BuildConfig.class, sdk = 21)
 public class ActivityTest {
 
+    private MainActivity landingActivity;
+    @Before
+    public void setUp() throws Exception {
+        landingActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
+
+    @Test
+    public void testLandingPageContainApplogo(){
+        ImageView applogo =  (ImageView) landingActivity.findViewById(R.id.applogoImageView);
+        assertThat(applogo).isNotNull();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+
+    }
 }
