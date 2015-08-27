b6fa26225242496c6a75715d54a2174bc4b2fc70
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-08-27 16:11:33 +0100

    [#101752284] App logo placeholder

--- app/src/main/java/com/andela/bark/MainActivity.java
+++ app/src/main/java/com/andela/bark/MainActivity.java
@@ -1,13 +1,20 @@
 package com.andela.bark;
 
 import android.app.Activity;
+<<<<<<< HEAD
 import android.content.Intent;
+=======
+import android.support.v7.app.ActionBarActivity;
+>>>>>>> e1dd5ff... App logo placeholder
 import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
+<<<<<<< HEAD
 
     private FacebookAuth facebookAuth = new FacebookAuth();
+=======
+>>>>>>> e1dd5ff... App logo placeholder
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
