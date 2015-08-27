274b24603c2be3cd9a781cdc031d8ad6f1b72e6d
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-08-27 14:51:21 +0100

    [#101752284] testLandingPage contains app log [pass]

--- app/src/test/java/com/andela/bark/MainActivityTest.java
+++ app/src/test/java/com/andela/bark/MainActivityTest.java
@@ -3,8 +3,14 @@ package com.andela.bark;
 /**
  * Created by andela-cj on 8/26/15.
  */
+<<<<<<< HEAD:app/src/test/java/com/andela/bark/MainActivityTest.java
 import android.app.Activity;
 
+=======
+import android.widget.ImageView;
+
+import org.junit.After;
+>>>>>>> 3409b2d... testLandingPage contains app log [pass]:app/src/test/java/com/andela/bark/ActivityTest.java
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,9 +26,30 @@ import static org.assertj.android.api.Assertions.assertThat;
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MainActivityTest {
 
+<<<<<<< HEAD:app/src/test/java/com/andela/bark/MainActivityTest.java
     @Test
     public void titleIsCorrect()  throws Exception{
         Activity activity = Robolectric.setupActivity(MainActivity.class);
         assertTrue(activity.getTitle().toString().equals("Gatekeepr"));
     }
 }
+=======
+    private MainActivity landingActivity;
+    @Before
+    public void setUp() throws Exception {
+        landingActivity = Robolectric.buildActivity(MainActivity.class).create().get();
+    }
+
+    @Test
+    public void testLandingPageContainApplogo(){
+        ImageView applogo =  (ImageView) landingActivity.findViewById(R.id.applogoImageView);
+        assertThat(applogo).isNotNull();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+
+    }
+}
+>>>>>>> 3409b2d... testLandingPage contains app log [pass]:app/src/test/java/com/andela/bark/ActivityTest.java
