0f33e591f330d9dbf55558b3dd161a4d770c1abe
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-08-28 13:16:53 +0100

    Google sign in achieved

--- app/src/main/java/com/andela/bark/MainActivity.java
+++ app/src/main/java/com/andela/bark/MainActivity.java
@@ -1,11 +1,15 @@
 package com.andela.bark;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -21,14 +25,13 @@ public class MainActivity extends Activity implements
 
     /* Request code used to invoke sign in user interactions. */
     private static final int RC_SIGN_IN = 0;
-
     /* Client used to interact with Google APIs. */
     private GoogleApiClient mGoogleApiClient;
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
 
-    @Override
-    public void onClick(View v) {
-
-    }
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
 
 
     @Override
@@ -46,8 +49,23 @@ public class MainActivity extends Activity implements
                 .addScope(new Scope(Scopes.PLUS_ME))
                 .build();
 
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("MainActivity", "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
 
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,7 +102,14 @@ public class MainActivity extends Activity implements
 
     @Override
     public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d("MainActivity", "onConnected:" + bundle);
+        mShouldResolve = false;
 
+        // Show the signed-in UI
+        Toast.makeText(this, "signed-in ", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -94,6 +119,50 @@ public class MainActivity extends Activity implements
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d("MainActivity", "onConnectionFailed:" + connectionResult);
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e("MainActivity", "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                Toast.makeText(this, "Connection could not be resolved", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Show the signed-out UI
+            Toast.makeText(this, "Signed-out", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_in_button) {
+            onSignInClicked();
+        }
+
+        // ...
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+//        mStatusTextView.setText(R.string.signing_in);
+        Toast.makeText(this, "Signing in", Toast.LENGTH_LONG).show();
 
     }
 }
