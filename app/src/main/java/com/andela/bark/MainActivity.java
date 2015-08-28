a96afa6822d4fc638789d89635f0831a6a6beac2
Toni Solarin-Sodara <toni.edward@outlook.com>
2015-08-28 12:30:15 +0100

    [#101752666] Added google api client initialization cade to Main activity

--- app/src/main/java/com/andela/bark/MainActivity.java
+++ app/src/main/java/com/andela/bark/MainActivity.java
@@ -7,14 +7,45 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 >>>>>>> e1dd5ff... App logo placeholder
 import android.os.Bundle;
+<<<<<<< HEAD
+=======
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+>>>>>>> 6c14333... Added google api client initialization cade to Main activity
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.plus.Plus;
 
 
+public class MainActivity extends Activity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
+
+    /* Request code used to invoke sign in user interactions. */
+    private static final int RC_SIGN_IN = 0;
+
+    /* Client used to interact with Google APIs. */
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+<<<<<<< HEAD
 public class MainActivity extends Activity {
 <<<<<<< HEAD
 
     private FacebookAuth facebookAuth = new FacebookAuth();
 =======
 >>>>>>> e1dd5ff... App logo placeholder
+=======
+>>>>>>> 6c14333... Added google api client initialization cade to Main activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +57,20 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
+<<<<<<< HEAD
         facebookAuth.loginButton(this);
+=======
+        // Build GoogleApiClient with access to basic profile
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.PLUS_LOGIN))
+                .addScope(new Scope(Scopes.PLUS_ME))
+                .build();
+
+>>>>>>> 6c14333... Added google api client initialization cade to Main activity
     }
 
     @Override
@@ -39,4 +83,35 @@ public class MainActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
         facebookAuth.onActivityResult(requestCode, resultCode, data);
     }
+<<<<<<< HEAD
 }
+=======
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+}
+>>>>>>> 6c14333... Added google api client initialization cade to Main activity
