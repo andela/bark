b9f8a0ef1b8365557578c6bb709aff1693f9d982
Joshua Ugba <justjosh001@gmail.com>
2015-08-31 22:58:24 +0100

    [#101752398] Set up fragment manager

--- app/src/main/java/com/andela/bark/FragmentContainerActivity.java
+++ app/src/main/java/com/andela/bark/FragmentContainerActivity.java
@@ -1,9 +1,29 @@
 package com.andela.bark;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by andela-jugba on 8/31/15.
  */
 public class FragmentContainerActivity extends FragmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragmentcontainer);
+        setUpFragment();
+    }
+
+    private void setUpFragment(){
+
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new EventListFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer,fragment).commit();
+        }
+    }
 }
