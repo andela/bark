42482656418aa73cf2b19f6d13d3edb542a44c0a
Joshua Ugba <justjosh001@gmail.com>
2015-08-31 21:13:01 +0100

    [#101752398] Add FragmentContainerActivity class

---
+++ app/src/main/java/com/andela/bark/FragmentContainerActivity.java
@@ -0,0 +1,9 @@
+package com.andela.bark;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by andela-jugba on 8/31/15.
+ */
+public class FragmentContainerActivity extends FragmentActivity{
+}
