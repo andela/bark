0dfb113b7db9374f730dac7ea27efebad4fdc25f
Joshua Ugba <justjosh001@gmail.com>
2015-08-31 21:22:06 +0100

    [#101752398] Add EventListFragment class

---
+++ app/src/main/java/com/andela/bark/EventListFragment.java
@@ -0,0 +1,19 @@
+package com.andela.bark;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by andela on 8/31/15.
+ */
+public class EventListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
