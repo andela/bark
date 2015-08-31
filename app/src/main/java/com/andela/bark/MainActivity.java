314b3552191767ee5639a625412c756817516df0
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-08-31 12:00:37 +0100

    [#101752666] Refactor GoogleApiClient -> GoogleHandler

--- app/src/main/java/com/andela/bark/MainActivity.java
+++ app/src/main/java/com/andela/bark/MainActivity.java
@@ -30,6 +30,7 @@ import android.view.View;
 import android.widget.Toast;
 >>>>>>> 0f33e59... Google sign in achieved
 
+import com.andela.bark.utils.Handlers.GoogleHandler;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
@@ -37,21 +38,10 @@ import com.google.android.gms.common.Scopes;
 import com.google.android.gms.plus.Plus;
 
 
-public class MainActivity extends Activity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        View.OnClickListener {
+public class MainActivity extends Activity {
 
-    /* Request code used to invoke sign in user interactions. */
-    private static final int RC_SIGN_IN = 0;
-    /* Client used to interact with Google APIs. */
-    private GoogleApiClient mGoogleApiClient;
-    /* Is there a ConnectionResult resolution in progress? */
-    private boolean mIsResolving = false;
 
-    /* Should we automatically resolve ConnectionResults when possible? */
-    private boolean mShouldResolve = false;
-
+<<<<<<< HEAD
 <<<<<<< HEAD
 public class MainActivity extends Activity {
 <<<<<<< HEAD
@@ -61,6 +51,9 @@ public class MainActivity extends Activity {
 >>>>>>> e1dd5ff... App logo placeholder
 =======
 >>>>>>> 6c14333... Added google api client initialization cade to Main activity
+=======
+    private GoogleHandler googleHandler;
+>>>>>>> 1338636... Refactor GoogleApiClient -> GoogleHandler
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +64,7 @@ public class MainActivity extends Activity {
         facebookAuth.trackers();
 
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
 
 <<<<<<< HEAD
         facebookAuth.loginButton(this);
@@ -89,10 +83,15 @@ public class MainActivity extends Activity {
 >>>>>>> 6c14333... Added google api client initialization cade to Main activity
 =======
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+=======
+        googleHandler = new GoogleHandler(this);
+>>>>>>> 1338636... Refactor GoogleApiClient -> GoogleHandler
     }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+<<<<<<< HEAD
         Log.d("MainActivity", "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
 
         if (requestCode == RC_SIGN_IN) {
@@ -115,6 +114,9 @@ public class MainActivity extends Activity {
     public void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
         facebookAuth.onActivityResult(requestCode, resultCode, data);
+=======
+        googleHandler.onActivityResult(requestCode, resultCode, data);
+>>>>>>> 1338636... Refactor GoogleApiClient -> GoogleHandler
     }
 <<<<<<< HEAD
 }
@@ -123,77 +125,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+        googleHandler.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mGoogleApiClient.disconnect();
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        // onConnected indicates that an account was selected on the device, that the selected
-        // account has granted any requested permissions to our app and that we were able to
-        // establish a service connection to Google Play services.
-        Log.d("MainActivity", "onConnected:" + bundle);
-        mShouldResolve = false;
-
-        // Show the signed-in UI
-        Toast.makeText(this, "signed-in ", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        // Could not connect to Google Play Services.  The user needs to select an account,
-        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
-        // ConnectionResult to see possible error codes.
-        Log.d("MainActivity", "onConnectionFailed:" + connectionResult);
-
-        if (!mIsResolving && mShouldResolve) {
-            if (connectionResult.hasResolution()) {
-                try {
-                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
-                    mIsResolving = true;
-                } catch (IntentSender.SendIntentException e) {
-                    Log.e("MainActivity", "Could not resolve ConnectionResult.", e);
-                    mIsResolving = false;
-                    mGoogleApiClient.connect();
-                }
-            } else {
-                // Could not resolve the connection result, show the user an
-                // error dialog.
-                Toast.makeText(this, "Connection could not be resolved", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            // Show the signed-out UI
-            Toast.makeText(this, "Signed-out", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.sign_in_button) {
-            onSignInClicked();
-        }
-    }
-
-    private void onSignInClicked() {
-        // User clicked the sign-in button, so begin the sign-in process and automatically
-        // attempt to resolve any errors that occur.
-        mShouldResolve = true;
-        mGoogleApiClient.connect();
-
-        // Show a message to the user that we are signing in.
-//        mStatusTextView.setText(R.string.signing_in);
-        Toast.makeText(this, "Signing in", Toast.LENGTH_LONG).show();
-
+        googleHandler.disconnect();
     }
 }
 >>>>>>> 6c14333... Added google api client initialization cade to Main activity
