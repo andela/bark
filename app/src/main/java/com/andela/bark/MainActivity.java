e89cda9037c02952cfef0b66cafc7368e1f90669
Toni Solarin-Sodara <toni.edward@outlook.com>
2015-08-31 11:01:35 +0100

    [#101752666] Now shows details of logged in user after authentication

--- app/src/main/java/com/andela/bark/MainActivity.java
+++ app/src/main/java/com/andela/bark/MainActivity.java
@@ -34,7 +34,16 @@ import android.widget.Toast;
 import android.os.Bundle;
 >>>>>>> d3a5108... Directory Restructure
 
+<<<<<<< HEAD
 import com.andela.bark.authentication.GoogleAuth;
+=======
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+>>>>>>> 35fe612... Now shows details of logged in user after authentication
 
 
 public class MainActivity extends Activity {
@@ -46,6 +55,7 @@ public class MainActivity extends Activity {
 public class MainActivity extends Activity {
 <<<<<<< HEAD
 
+<<<<<<< HEAD
     private FacebookAuth facebookAuth = new FacebookAuth();
 =======
 >>>>>>> e1dd5ff... App logo placeholder
@@ -57,6 +67,10 @@ public class MainActivity extends Activity {
 =======
     private GoogleAuth googleHandler;
 >>>>>>> d3a5108... Directory Restructure
+=======
+    /* Object used to hold logged in user info */
+    private Person person;
+>>>>>>> 35fe612... Now shows details of logged in user after authentication
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,7 +152,77 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
+<<<<<<< HEAD
         googleHandler.disconnect();
+=======
+        mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d("MainActivity", "onConnected:" + bundle);
+        mShouldResolve = false;
+
+        person =  Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+
+        // Show welcome message
+        Toast.makeText(this, "Welcome " + person.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d("MainActivity", "onConnectionFailed:" + connectionResult);
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e("MainActivity", "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                Toast.makeText(this, "Connection could not be resolved", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            // Show the signed-out UI
+            Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_in_button) {
+            onSignInClicked();
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+        // Show a message to the user that we are signing in.
+//        mStatusTextView.setText(R.string.signing_in);
+        Toast.makeText(this, "Signing in", Toast.LENGTH_LONG).show();
+
+>>>>>>> 35fe612... Now shows details of logged in user after authentication
     }
 }
 >>>>>>> 6c14333... Added google api client initialization cade to Main activity
