172d1a96304dc4456396ea04217742ada9a81b38
Joshua Ugba <justjosh001@gmail.com>
2015-10-07 14:42:12 +0100

    Refactor

--- app/src/main/java/com/andela/bark/fragments/EventDetailFragment.java
+++ app/src/main/java/com/andela/bark/fragments/EventDetailFragment.java
@@ -56,6 +56,14 @@ public class EventDetailFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "Dashboard Button Clicked", Toast.LENGTH_SHORT).show();
+                EventDashBoardFragment eventDashBoardFragment = new EventDashBoardFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                eventDashBoardFragment.setArguments(args);
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, eventDashBoardFragment)
+                        .addToBackStack(null)
+                        .commit();
             }
         });
 
