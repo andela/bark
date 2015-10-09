891b6bf95b75af3c5859302e35536378e5811d53
Joshua Ugba <justjosh001@gmail.com>
2015-10-09 17:06:20 +0100

    [#101752998] refactor to use parse object

--- app/src/main/java/com/andela/bark/GateKeeperManager.java
+++ app/src/main/java/com/andela/bark/GateKeeperManager.java
@@ -2,10 +2,9 @@ package com.andela.bark;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.util.Log;
 
+import com.andela.bark.models.Privilege;
 import com.andela.bark.models.User;
-import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -17,8 +16,7 @@ import java.util.List;
  * Created by andela-cj on 9/18/15.
  */
 public class GateKeeperManager {
-    private User keeper;
-    private Role GKprRole;
+    private static User keeper;
     public boolean isAuthenticated;
     private Activity activity;
 
@@ -34,9 +32,9 @@ public class GateKeeperManager {
         getUserRoles();
     }
 
-    public void authenticate(final ParseObject role) {
+    public void authenticate(final Privilege role) {
         ProgressDialog dialog = new ProgressDialog(activity);
-        ParseQuery<ParseObject> usersQuery = ParseQuery.getQuery("Users");
+        ParseQuery<ParseObject> usersQuery = ParseQuery.getQuery("User");
         usersQuery.whereEqualTo("userID", keeper.getUserID()).include("role");
 
         dialog.setMessage("Fetching...");
@@ -46,12 +44,13 @@ public class GateKeeperManager {
             if (dialog.isShowing()) dialog.dismiss();
 
             if (list!= null && list.size() == 0) {
-                createUser(role);
+                createUserWithPrivilege();
                 isAuthenticated = true;
             } else {
-                ParseObject user = list.remove(0);
-                ParseObject userRole = user.getParseObject("role");
-                if ((GKprRole = validateRole(userRole.getString("name"))) != null) {
+                User user = (User) list.remove(0);
+                Privilege userRole = (Privilege)user.getParseObject("role");
+                if ( userRole.getObjectId().equals(role.getObjectId())) {
+                    keeper = user;
                     isAuthenticated = true;
                 }
             }
@@ -65,43 +64,25 @@ public class GateKeeperManager {
         ParseQuery<ParseObject> rolesQuery = ParseQuery.getQuery("Privilege");
         try {
             List<ParseObject> list = rolesQuery.find();
-            authenticate(list.get(0));
+            authenticate((Privilege)list.get(0));
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
 
-    public void createUser(final ParseObject role) {
-        ParseObject user = new ParseObject("Users");
-        user.put("FirstName", keeper.getFirstName());
-        user.put("LastName", keeper.getLastName());
-        user.put("role", role);
-        user.put("userID", keeper.getUserID());
-        user.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    keeper.setRole(role.getString("name"));
-                }
-            }
-        });
+    public void createUserWithPrivilege() throws ParseException {
+        ParseQuery<ParseObject> roleQuery = ParseQuery.getQuery("Privilege");
+        List <ParseObject> list = roleQuery.whereEqualTo("name","Admin").find();
+        Privilege role = (Privilege) list.remove(0);
+        keeper.put("role", role);
+        keeper.saveInBackground();
     }
 
 
-    public Role validateRole(String role){
-        Role eRole;
-        switch (role){
-            case "Admin":
-                eRole = Role.Admin; break;
-            case "Manager":
-                eRole = Role.Manager; break;
-            case "keeper":
-                eRole = Role.GateKeeper;
-                break;
-            default: eRole = null; break;
-        }
-        return eRole;
+    public static User getKeeper(){
+        return keeper;
     }
+
 
     private enum Role{
         Admin, Manager, GateKeeper;
