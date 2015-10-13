eb88042f18e044bfb81c4aa01dbb87ff3e475c03
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-13 08:55:03 +0100

    Implement smiley feedback for ticket verification

--- app/src/main/java/com/andela/bark/ticketVerification/TicketValidator.java
+++ app/src/main/java/com/andela/bark/ticketVerification/TicketValidator.java
@@ -1,18 +1,24 @@
 package com.andela.bark.ticketVerification;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
+import android.app.Dialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.andela.bark.GateKeeperManager;
+import com.andela.bark.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -33,45 +39,27 @@ public class TicketValidator {
         query.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
+                Dialog smiley;
                 if (e == null) {
                     if (list.size() == 1) {
                         ParseObject ticket = list.get(0);
                         Boolean used = ticket.getBoolean("used");
                         if (used) {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                            builder.setMessage("Ticket Has Been Used!")
-                                    .setCancelable(false)
-                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            //do things
-                                            restartScanner();
-                                        }
-                                    });
-                            AlertDialog alert = builder.create();
-                            alert.show();
+                            smiley = BuildSmileyDialog("Ticket Has Been Used!", R.drawable.frown, Color.RED);
+                            smiley.show();
                         } else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                            builder.setMessage("Valid Ticket!")
-                                    .setCancelable(false)
-                                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog, int id) {
-                                            //do things
-                                            restartScanner();
-                                        }
-                                    });
-                            AlertDialog alert = builder.create();
-                            alert.show();
+                            smiley = BuildSmileyDialog("Valid Ticket!", R.drawable.smile, Color.GREEN);
+                            smiley.setOnDismissListener(dismissListener);
+                            smiley.show();
                             ticket.put("scannedBy", GateKeeperManager.getKeeper());
                             ticket.put("used", true);
                             ticket.saveInBackground();
                         }
                     } else {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                        builder.setMessage("Ticket NOT Valid!")
-                                .setCancelable(false)
-                                .setPositiveButton("OK", null);
-                        AlertDialog alert = builder.create();
-                        alert.show();
+                        smiley = BuildSmileyDialog("Ticket NOT Valid!", R.drawable.frown, Color.RED);
+                        smiley.setOnDismissListener(dismissListener);
+                        smiley.show();
+
                     }
                 } else {
                     Log.d("Error Message", e.getMessage());
@@ -83,5 +71,30 @@ public class TicketValidator {
     private void restartScanner(){
         activity.finish();
         activity.startActivity(activity.getIntent());
+    }
+
+
+    DialogInterface.OnDismissListener dismissListener = new DialogInterface.OnDismissListener() {
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+        // restartScanner();
+
+        }
+    };
+    private Dialog BuildSmileyDialog(String message, int resourceId, int color){
+        Dialog a = new Dialog(activity);
+        a.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        a.setContentView(R.layout.smile);
+        PorterDuff.Mode mMode = PorterDuff.Mode.SRC_IN;
+        ImageView f = ((ImageView) a.findViewById(R.id.smileImage));
+        TextView v = (TextView) a.findViewById(R.id.message);
+        Drawable ojo = activity.getResources().getDrawable(resourceId);
+        if (ojo !=null){
+            ojo.setColorFilter(color, mMode);
+            f.setImageDrawable(ojo);
+            a.getWindow().setBackgroundDrawable(new ColorDrawable(Color.LTGRAY));
+            v.setText(message);
+        }
+        return a;
     }
 }
