294efbc3368b7b40608bdbaafa8d2162f3d64ac2
andela-cndubisi <chijioke.ndubisi@andela.co>
2015-10-26 17:13:43 +0100

    Create Event model inorder to permit casting

--- app/src/main/java/com/andela/bark/models/Events.java
+++ app/src/main/java/com/andela/bark/models/Events.java
@@ -1,14 +1,18 @@
 package com.andela.bark.models;
 
 import com.parse.FindCallback;
+import com.parse.ParseClassName;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by andela-cj on 26/10/2015.
  */
+@ParseClassName("Events")
 public class Events extends ParseObject {
     private static String CLASSNAME = "Events";
 
@@ -23,7 +27,15 @@ public class Events extends ParseObject {
             @Override
             public void done(List<ParseObject> list, ParseException e) {
                 if (list != null) {
-                    callback.onSuccess(list);
+                    ArrayList<Events> events = new ArrayList<Events>();
+                    for (ParseObject ob: list ) {
+                        Events a =  new Events();
+                        a.put("Name", ob.get("Name"));
+                        a.put("Location", ob.get("Location"));
+                        a.setObjectId(a.getObjectId());
+                        events.add(a);
+                    }
+                    callback.onSuccess(events);
                 } else {
                     Exception exception = new Exception("List is Null");
                     callback.onError(exception);
